da4b4acc0c07e971e308205719fed8cace4f98aa
Techstar. <akoredebolujoko@gmail.com>
2025-03-27 15:47:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Setup/', // Ensure this matches your GitHub repository name
-  server: {
-    port: 3000, // Optional: Set your development server port
-  },
 });
